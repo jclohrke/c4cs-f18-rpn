b90396132487c8b8711b9eeff97c2948685ef395
Josh Lohrke <jclohrke@umich.edu>
2018-11-07 12:11:58 -0500

    Added multiplicationa dn division

--- test_rpn.py
+++ test_rpn.py
@@ -10,6 +10,14 @@ class TestBasics(unittest.TestCase):
 		result = rpn.calculate('4 3 -')
 		self.assertEqual(1, result)
 
+	def test_mul(self):
+		result = rpn.calculate('4 3 *')
+		self.assertEqual(12, result)
+
+	def test_div(self):
+		result = rpn.calculate('4 2 /')
+		self.assertEqual(2, result)
+
 # TODO: Add a test for subtract
 
 
